7a896e98128004431c1a964b28a0b0493af3f008
kemetmed <119786084+kemetmed@users.noreply.github.com>
2022-12-07 11:24:18 +0200

    Initialize newFix

---
+++ Hackfest/doctor/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.doctorHome),
+    path('/patient-list/', views.patientList),
+    path('/patient-list/patient/<int:patientId>', views.patientRecord,name='patientDiagnosis'),
+    path('/viewPrescription/<int:prescriptionId>', views.viewPrescription, name="viewPrescription"),
+    path('/diagnosis/<int:patientId>', views.diagnosis),
+    path('/medication/<int:prescriptionId>', views.medication),
+    path('/searchpage/', views.searchPatient, name='searchlist'),
+    path('/medicineFile/<int:prescriptionId>', views.medicineFile, name='medicineFile'),
+    path('/laboratoryTest/<int:prescriptionId>/<str:message>', views.laboratoryTest, name='laboratoryTest')
+
+]
